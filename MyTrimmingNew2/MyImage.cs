e7094e8bed34c14018e6b3a14ca47fa57892a14c
FourHyphen <FourHyphen.4@gmail.com>
2021-02-26 08:19:26 +0900

    画面に表示するimageの作成処理を実装

---
+++ MyTrimmingNew2/MyImage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Media;
+
+namespace MyTrimmingNew2
+{
+    public class MyImage
+    {
+        /// <summary>
+        /// 表示用画像
+        /// </summary>
+        /// <returns></returns>
+        public static System.Windows.Media.Imaging.BitmapSource GetShowImage(string imagePath)
+        {
+            Bitmap bitmap = new Bitmap(imagePath);
+            return CreateBitmapSourceImage(bitmap);
+        }
+
+        [System.Runtime.InteropServices.DllImport("gdi32.dll", EntryPoint = "DeleteObject")]
+        public static extern bool DeleteObject(IntPtr hObject);
+
+        private static System.Windows.Media.Imaging.BitmapSource CreateBitmapSourceImage(Bitmap bitmapImage)
+        {
+            // 参考: http://qiita.com/KaoruHeart/items/dc130d5fc00629c1b6ea
+            IntPtr handle = bitmapImage.GetHbitmap();
+            try
+            {
+                return System.Windows.Interop.Imaging.
+                    CreateBitmapSourceFromHBitmap(handle,
+                                                  IntPtr.Zero,
+                                                  System.Windows.Int32Rect.Empty,
+                                                  System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally
+            {
+                DeleteObject(handle);
+            }
+        }
+    }
+}
---
+++ TestMyTrimmingNew2/TestImageProcess.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestMyTrimmingNew2
+{
+    [TestClass]
+    public class TestImageProcess
+    {
+        private string BeforeEnvironment { get; set; }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            BeforeEnvironment = Environment.CurrentDirectory;
+            Environment.CurrentDirectory = Common.GetEnvironmentDirPath();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            Environment.CurrentDirectory = BeforeEnvironment;
+        }
+
+        [TestMethod]
+        public void TestSuccessOfCreateShowImage()
+        {
+            string imagePath = Common.GetFilePathOfDependentEnvironment("/Resource/test001.jpg");
+            System.Windows.Media.Imaging.BitmapSource bs = MyTrimmingNew2.MyImage.GetShowImage(imagePath);
+            Assert.AreEqual(expected: 3840, actual: bs.PixelWidth);
+            Assert.AreEqual(expected: 2560, actual: bs.PixelHeight);
+        }
+    }
+}
