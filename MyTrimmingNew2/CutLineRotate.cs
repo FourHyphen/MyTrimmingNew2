1fb8b9d64669e8bad236d00f3ddf4dfafd1a2ce4
FourHyphen <FourHyphen.4@gmail.com>
2021-03-20 08:41:14 +0900

    リファクタ：不要なコードを削除

--- MyTrimmingNew2/CutLineRotate.cs
+++ MyTrimmingNew2/CutLineRotate.cs
@@ -5,16 +5,10 @@ namespace MyTrimmingNew2
 {
     public class CutLineRotate : CutLineCommand
     {
-        private double MaxRight { get; }
-
-        private double MaxBottom { get; }
-
         private double RotateDegree { get; set; }
 
         public CutLineRotate(CutLine cutLine, ShowingImage image, Key key, int keyInputNum) : base(cutLine)
         {
-            MaxRight = image.Width;
-            MaxBottom = image.Height;
             CalcRotateDegree(key, keyInputNum);
         }
 
@@ -41,21 +35,14 @@ namespace MyTrimmingNew2
 
         private CutLineParameter CalcRotate()
         {
-            //double r = CalcR();
-            //System.Windows.Point newLeftTop = CalcLeftTop(Before.ThetaDegree, RotateDegree);
-            //System.Windows.Point newRightTop = RightTop(Before.ThetaDegree, RotateDegree);
-            //System.Windows.Point newRightBottom = RightBottom(Before.ThetaDegree, RotateDegree);
-            //System.Windows.Point newLeftBottom = LeftBottom(Before.ThetaDegree, RotateDegree);
-            //return new CutLineParameter(newLeftTop, newRightTop, newRightBottom, newLeftBottom, Before.Degree + RotateDegree);
-
             double centerX = CalcCenterX(Before.LeftTop, Before.RightBottom);
             double centerY = CalcCenterY(Before.LeftBottom, Before.RightTop);
+            double rotateRad = ToRadian(RotateDegree);
 
-            // メッセージ：計算結果が何かズレてる、少なくともWidthは回転前からズレる
-            System.Windows.Point newLeftTop = CalcRotatePoint(Before.LeftTop, centerX, centerY, RotateDegree);
-            System.Windows.Point newRightTop = CalcRotatePoint(Before.RightTop, centerX, centerY, RotateDegree);
-            System.Windows.Point newRightBottom = CalcRotatePoint(Before.RightBottom, centerX, centerY, RotateDegree);
-            System.Windows.Point newLeftBottom = CalcRotatePoint(Before.LeftBottom, centerX, centerY, RotateDegree);
+            System.Windows.Point newLeftTop = CalcRotatePoint(Before.LeftTop, centerX, centerY, rotateRad);
+            System.Windows.Point newRightTop = CalcRotatePoint(Before.RightTop, centerX, centerY, rotateRad);
+            System.Windows.Point newRightBottom = CalcRotatePoint(Before.RightBottom, centerX, centerY, rotateRad);
+            System.Windows.Point newLeftBottom = CalcRotatePoint(Before.LeftBottom, centerX, centerY, rotateRad);
             return new CutLineParameter(newLeftTop, newRightTop, newRightBottom, newLeftBottom, Before.Degree + RotateDegree);
         }
 
@@ -69,9 +56,8 @@ namespace MyTrimmingNew2
             return (p1.Y + p2.Y) / 2.0;
         }
 
-        private System.Windows.Point CalcRotatePoint(System.Windows.Point p, double centerX, double centerY, double degree)
+        private System.Windows.Point CalcRotatePoint(System.Windows.Point p, double centerX, double centerY, double rad)
         {
-            double rad = ToRadian(degree);
             double x = p.X - centerX;
             double y = p.Y - centerY;
             double rotateX = x * Math.Cos(rad) - y * Math.Sin(rad);
@@ -83,69 +69,5 @@ namespace MyTrimmingNew2
         {
             return degree * Math.PI / 180.0;
         }
-
-        //public double CalcR()
-        //{
-        //    return Math.Sqrt(Math.Pow(Before.LeftTop.X - CenterX, 2) + Math.Pow(Top - CenterY, 2));
-        //}
-
-        //private System.Windows.Point CalcLeftTop(double thetaDegree, double rotateDegree)
-        //{
-        //    double thetaMinus = thetaDegree - rotateDegree;
-
-        //    // LeftTop -> -180 <= Degree < -90
-        //    double newX = Before.R * Math.Cos(ToRadian(thetaMinus - 180)) + Before.CenterX;
-        //    double newY = Before.R * Math.Sin(ToRadian(thetaMinus - 180)) + Before.CenterY;
-        //    return new System.Windows.Point(newX, newY);
-        //}
-
-        //private System.Windows.Point RightTop()
-        //{
-        //    // RightTop -> -90 <= Degree < 0
-        //    double theta = ThetaBase - Degree - 90;
-        //    double newX = R * Math.Cos(ToRadian(theta)) + CenterX;
-        //    double newY = R * Math.Sin(ToRadian(theta)) + CenterY;
-        //    return new System.Windows.Point(newX, newY);
-        //}
-
-        //private System.Windows.Point RightBottom()
-        //{
-        //    // RightBottom -> 0 <= Degree < 90
-        //    double theta = ThetaBase + Degree;
-        //    double newX = R * Math.Cos(ToRadian(theta)) + CenterX;
-        //    double newY = R * Math.Sin(ToRadian(theta)) + CenterY;
-        //    return new System.Windows.Point(newX, newY);
-        //}
-
-        //private System.Windows.Point LeftBottom()
-        //{
-        //    // LeftBottom -> 90 <= Degree < 180
-        //    double theta = 180 - (ThetaBase - Degree);
-        //    double newX = R * Math.Cos(ToRadian(theta)) + CenterX;
-        //    double newY = R * Math.Sin(ToRadian(theta)) + CenterY;
-        //    return new System.Windows.Point(newX, newY);
-        //}
-
-        //private void SetTheta()
-        //{
-        //    double rad = Math.Atan((Top - CenterY) / (Left - CenterX));
-        //    ThetaBase = (180.0 * rad) / Math.PI;
-        //}
-
-        //private void SetCenter()
-        //{
-        //    CenterX = (RightBottom.X + LeftTop.X) / 2.0;
-        //    CenterY = (LeftBottom.Y + RightTop.Y) / 2.0;
-        //}
-
-        //private double ThetaBase { get; set; }
-
-        //public double ThetaDegree
-        //{
-        //    get
-        //    {
-        //        return ThetaBase + Degree;
-        //    }
-        //}
     }
 }
