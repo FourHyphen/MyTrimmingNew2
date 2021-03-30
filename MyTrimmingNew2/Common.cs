c538c2bbc87b19e85645c8978de64513612affd6
FourHyphen <FourHyphen.4@gmail.com>
2021-03-31 07:53:18 +0900

    リファクタ：画像の回転後切り抜き保存に使用する機能を共通化

---
+++ MyTrimmingNew2/Common.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTrimmingNew2
+{
+    public class Common
+    {
+        private Common() { }
+
+        public static double CalcCenterX(System.Windows.Point p1, System.Windows.Point p2)
+        {
+            return (p1.X + p2.X) / 2.0;
+        }
+
+        public static double CalcCenterY(System.Windows.Point p1, System.Windows.Point p2)
+        {
+            return (p1.Y + p2.Y) / 2.0;
+        }
+
+        public static double ToRadian(double degree)
+        {
+            return degree * Math.PI / 180.0;
+        }
+
+        public static System.Windows.Point CalcRotatePoint(System.Windows.Point p, double centerX, double centerY, double rad)
+        {
+            double cos = Math.Cos(rad);
+            double sin = Math.Sin(rad);
+            return CalcRotatePoint(p, centerX, centerY, cos, sin);
+        }
+
+        public static System.Windows.Point CalcRotatePoint(System.Windows.Point p, double centerX, double centerY, double cos, double sin)
+        {
+            double x = p.X - centerX;
+            double y = p.Y - centerY;
+            double rotateX = x * cos - y * sin;
+            double rotateY = y * cos + x * sin;
+            return new System.Windows.Point(rotateX + centerX, rotateY + centerY);
+        }
+    }
+}
--- MyTrimmingNew2/CutLineRotate.cs
+++ MyTrimmingNew2/CutLineRotate.cs
@@ -45,14 +45,14 @@ namespace MyTrimmingNew2
 
         private CutLineParameter CalcRotate()
         {
-            double centerX = CalcCenterX(Before.LeftTop, Before.RightBottom);
-            double centerY = CalcCenterY(Before.LeftBottom, Before.RightTop);
-            double rotateRad = ToRadian(RotateDegree);
+            double centerX = Common.CalcCenterX(Before.LeftTop, Before.RightBottom);
+            double centerY = Common.CalcCenterY(Before.LeftBottom, Before.RightTop);
+            double rotateRad = Common.ToRadian(RotateDegree);
 
-            System.Windows.Point newLeftTop = CalcRotatePoint(Before.LeftTop, centerX, centerY, rotateRad);
-            System.Windows.Point newRightTop = CalcRotatePoint(Before.RightTop, centerX, centerY, rotateRad);
-            System.Windows.Point newRightBottom = CalcRotatePoint(Before.RightBottom, centerX, centerY, rotateRad);
-            System.Windows.Point newLeftBottom = CalcRotatePoint(Before.LeftBottom, centerX, centerY, rotateRad);
+            System.Windows.Point newLeftTop = Common.CalcRotatePoint(Before.LeftTop, centerX, centerY, rotateRad);
+            System.Windows.Point newRightTop = Common.CalcRotatePoint(Before.RightTop, centerX, centerY, rotateRad);
+            System.Windows.Point newRightBottom = Common.CalcRotatePoint(Before.RightBottom, centerX, centerY, rotateRad);
+            System.Windows.Point newLeftBottom = Common.CalcRotatePoint(Before.LeftBottom, centerX, centerY, rotateRad);
 
             if (DoStickOutImage(newLeftTop, newRightTop, newRightBottom, newLeftBottom))
             {
@@ -60,30 +60,6 @@ namespace MyTrimmingNew2
             }
 
             return new CutLineParameter(newLeftTop, newRightTop, newRightBottom, newLeftBottom, Before.Degree + RotateDegree);
-        }
-
-        private double CalcCenterX(System.Windows.Point p1, System.Windows.Point p2)
-        {
-            return (p1.X + p2.X) / 2.0;
-        }
-
-        private double CalcCenterY(System.Windows.Point p1, System.Windows.Point p2)
-        {
-            return (p1.Y + p2.Y) / 2.0;
-        }
-
-        private System.Windows.Point CalcRotatePoint(System.Windows.Point p, double centerX, double centerY, double rad)
-        {
-            double x = p.X - centerX;
-            double y = p.Y - centerY;
-            double rotateX = x * Math.Cos(rad) - y * Math.Sin(rad);
-            double rotateY = y * Math.Cos(rad) + x * Math.Sin(rad);
-            return new System.Windows.Point(rotateX + centerX, rotateY + centerY);
-        }
-
-        private double ToRadian(double degree)
-        {
-            return degree * Math.PI / 180.0;
         }
     }
 }
